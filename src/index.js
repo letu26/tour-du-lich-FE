8814b8b9b129a74e7d11b3649cb5815aab37cd7e
Tú <tu260203@gmail.com>
2025-06-08 19:26:01 +0700

    last update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@ant-design/v5-patch-for-react-19';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import allReducers from "./reducers";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+const store = createStore(allReducers);
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
